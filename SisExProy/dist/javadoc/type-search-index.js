8ece142a3da9c3b1ad6fc36dcda8e4dee4ad8b06
GatoGatinX <rasiulesoj@gmail.com>
2022-02-26 00:51:54 -0600

    Checar librerias -error- agregar usuarios

---
+++ SisExProy/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"Ventanas","l":"Administrador"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"conexion","l":"conexion"},{"p":"Ventanas","l":"Inicial"},{"p":"Ventanas","l":"login"},{"p":"Ventanas","l":"registro"},{"p":"Ventanas","l":"Usuario"}];updateSearchResults();
